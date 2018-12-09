30ce4b2daa884f29769be3541ea97605a114de22
Crainer <camogan3000@hotmail.com>
2018-12-08 20:01:07 -0500

    b

--- src/componentes/Hosting/hostings.vue
+++ src/componentes/Hosting/hostings.vue
@@ -1,3 +1,94 @@
-<template>
-  <div></div>
+<style lang="css">
+
+.box_pad {
+    position: absolute;
+    align-content: center;
+    padding: 10px;
+    align-items: center;
+    font-weight: bold;
+    width: 100%;
+    left: 0;
+    top: 10%;
+    background-color: rgba(75, 138, 244, 0.75);
+    text-align: center;
+}
+</style>
+
+<template lang="html">
+  <div>
+    <hostingPlanes :parametros="parametros" :cards="datos"></hostingPlanes>
+
+    <tablaContenido :parametros="parametros" :cards="datos"></tablaContenido>
+  </div>
 </template>
+
+<script>
+import tablaContenido from "./../UI/tabla_contenido.vue";
+import hostingPlanes from "./hosting_planes.vue";
+export default {
+  components: {
+    hostingPlanes,
+    tablaContenido
+  },
+  data() {
+    return {
+      parametros: [
+        "Almacenamiento",
+        "Banda Ancha",
+        "Dominio",
+        "SSL",
+        "Base de Datos",
+        "SiteBuilder",
+        "Email",
+        "Soporte técnico "
+      ],
+      datos: [
+        {
+          title: "PERSONAL",
+          src:
+            "https://www.nopcommerce.com/images/blog/17-April-A-guide-on-selecting-a-profitable-product-to-sell-online---Part-1/business_idea_pic.jpg",
+          flex: 12,
+          text: "Características",
+          propiedades: ["3GB", "Ilimitada", 2, true, 1, false, 3, "24/7"],
+          precio: {
+            mes: 3.75,
+            año: 39
+          }
+        },
+        {
+          title: "PYMES",
+          src:
+            "https://www.wardsauto.com/sites/wardsauto.com/files/uploads/2015/12/online-car-buying.jpg",
+          flex: 12,
+          text: "Características",
+          propiedades: ["7GB", "Ilimitada", 5, true, 2, true, 7, "24/7"],
+          precio: {
+            mes: 5,
+            año: 51
+          }
+        },
+        {
+          title: "ENTERPRISE",
+          src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+          flex: 12,
+          text: "Características",
+          propiedades: [
+            "25GB",
+            "Ilimitada",
+            "Ilimitada",
+            true,
+            "Ilimitada",
+            true,
+            "Ilimitada",
+            "24/7"
+          ],
+          precio: {
+            mes: 7.5,
+            año: 75
+          }
+        }
+      ]
+    };
+  }
+};
+</script>
---
+++ src/componentes/UI/list.vue
@@ -0,0 +1,34 @@
+<style scoped>
+.border_hover:hover {
+  border-left: 7px solid black;
+  background-color: rgba(150, 150, 150, 0.2);
+  color: black;
+  font-weight: bold;
+}
+</style>
+
+<template>
+  <v-list dense>
+    <v-list-tile
+      class="border_hover"
+      v-for="(menu, index) in items"
+      :key="index"
+      @click=""
+      :to="path[index]"
+      ripple
+    >
+      <v-list-tile-title>{{ menu }}</v-list-tile-title>
+    </v-list-tile>
+  </v-list>
+</template>
+
+<script>
+export default {
+  props: ["items", "path"],
+  data() {
+    return {
+      tab: null
+    };
+  }
+};
+</script>

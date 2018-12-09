4cbf61920c0abfe2c55de6152c1d754e7feb813a
Crainer <camogan3000@hotmail.com>
2018-12-08 19:50:40 -0500

    Initial commit

---
+++ src/-added.js
@@ -0,0 +1,9 @@
+import Vue from "vue";
+
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+/*import VDateRange from "vuetify-daterange-picker";
+import "vuetify-daterange-picker/dist/vuetify-daterange-picker.css";*/
+
+Vue.use(Vuetify);
+/*Vue.use(VDateRange);*/
---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app>
+    <router-view> </router-view>
+    <FooterW></FooterW>
+  </v-app>
+</template>
+
+<script>
+//import Headerw from "./componentes/header.vue";
+import FooterW from "./components/footer.vue";
+//import Carusel from "./componentes/Inicio/carusel.vue";
+//import Hosting from "./componentes/hosting/hostings.vue";
+//import datePicker from "./componentes/date-picker.vue";
+//import subtab from "./componentes/subtab.vue";
+export default {
+  components: {
+    //Headerw,
+    FooterW
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
